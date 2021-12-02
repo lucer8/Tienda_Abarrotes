5e5bd889532320ef7e42b5cb59f255124ac2ad09
lucer8 <50811580+lucer8@users.noreply.github.com>
2021-12-02 00:46:34 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,4 +1,5 @@
 <?php
-    $con=mysqli_connect("localhost","root","");
-    mysqli_select_db($con,"crud");
+ $con=mysqli_connect("localhost","root","root");
+ mysqli_select_db($con,"tienda_abarrotes");
+
 ?>
