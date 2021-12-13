68f6dfe901081f47bc7fe2d9798405be61d5ae70
lucer8 <50811580+lucer8@users.noreply.github.com>
2021-12-13 11:36:39 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
- $con=mysqli_connect("localhost","root","root","Tienda_Abarrotes");
+ $con=mysqli_connect("wet-php","root","root","Tienda_Abarrotes");
  if(!$con){
      echo "Error:No se pudo conectar a MySQL." .PHP_EOL;
      echo "errno de depuración: ".mysqli_connect_errno() .PHP_EOL;
